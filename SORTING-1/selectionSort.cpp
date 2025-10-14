9aedd8e4c9bc1ad97c320cd2436ee19932139154
marve01-hub <parteek.marve09@gmail.com>
2025-10-14 20:27:17 +0530

    *selection sort*

--- SORTING-1/selectionSort.cpp
+++ SORTING-1/selectionSort.cpp
@@ -26,35 +26,3 @@ int main(){
     
     
 }
-// #include <iostream>
-// using namespace std;
-
-// int main() {
-//     int arr[100], n;
-//     cin >> n;
-
-//     for (int i = 0; i < n; i++) {
-//         cin >> arr[i];
-//     }
-
-//     for (int i = 0; i < n - 1; i++) {
-//         int min = i;
-//         for (int j = i + 1; j < n; j++) {
-//             if (arr[j] < arr[min]) {
-//                 min = j;
-//             }
-//         }
-
-//         // Swap
-//         int temp = arr[min];
-//         arr[min] = arr[i];
-//         arr[i] = temp;
-//     }
-
-//     // Print sorted array
-//     for (int i = 0; i < n; i++) {
-//         cout << arr[i] << " ";
-//     }
-
-//     return 0;
-// }
