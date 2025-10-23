72b05ecc805f384e306102e7fdc871db7fc0d480
marve01-hub <parteek.marve09@gmail.com>
2025-10-24 01:42:25 +0530

    Merge Sort

--- EXTRAconcept/Intersection.cpp
+++ EXTRAconcept/Intersection.cpp
@@ -4,18 +4,25 @@
 //2, 1, 2, 3, 4, 6
 //result => 2, 2, 1 , 4, 6
 #include<iostream>
+#include<set>
 using namespace std;
 int main(){
+
+    set<int> s;
+
     int a1[6] = {2, 2, 1, 4, 5, 6};
-    int a2[6] = {2, 1, 2, 3, 4, 6};
+    int a2[6] = {2, 2, 2, 3, 4, 6};
     for(int i = 0;i<6;i++){
-        for(int j = 0;j<6;j++){
-            if(a2[j] == a1[i]){
-             cout<<a2[j]<<" ";
-              break;
-                
-            }
-        }
+        if(a1[i]!=a1[i+1])
+        s.insert(a1[i]);
     }
+    for(int i = 0;i<6;i++){
+        s.insert(a2[i]);
+    }
+    for(auto &x: s){
+        cout<<x<<" ";
+        
+    }
+    
 
 }
--- SORTING-1/MergeSort.cpp
+++ SORTING-1/MergeSort.cpp
@@ -2,25 +2,26 @@
 using namespace std;
 void merge(int arr[], int low, int high)
 {
-    if (low >= high)
+    if (low >= high) //* Base case >> to know when to stop dividing array(hypothetical) into left and right arr
         return;
 
     int mid = low + (high - low) / 2; //! int overflow safety method**
-    merge(arr, low, mid);
-    merge(arr, mid + 1, high);
+    merge(arr, low, mid);//* for left array
+    merge(arr, mid + 1, high);//* for right array
 
-    int size_arr = high - low + 1;
-    int temp_arr[size_arr];
-    int ptr_1 = low;
-    int ptr_2 = mid + 1;
-    int start_index = 0;
+    // TODO: Merge Sort
+    int size_arr = high - low + 1; // *! if we don't know size        
+    int temp_arr[size_arr]; //* created a temporary array that contains sorted arr[hypo]
+    int ptr_1 = low;//! starting pinter of left arr
+    int ptr_2 = mid + 1;//! starting pinter of left arr
+    int start_index = 0;//* seted temporary array's index to 0
     while(ptr_1 <= mid and ptr_2 <= high){
-        if(arr[ptr_1]<= arr[ptr_2]){
-            temp_arr[start_index] = arr[ptr_1];
+        if(arr[ptr_1]<= arr[ptr_2]){   // if left's idx < right's idx means left element is smaller and must be before
+            temp_arr[start_index] = arr[ptr_1];  //* temp arr ke currnt idx pe snalleer to bigger element are being placed
             ptr_1++;
 
         }
-        else{
+        else{  // if right idx element < left idx element
             temp_arr[start_index] =arr[ptr_2];
             ptr_2++;
         }
@@ -29,21 +30,21 @@ void merge(int arr[], int low, int high)
         start_index++;
     }
 
-    while(ptr_1 <= mid){
+    while(ptr_1 <= mid){ //! if the left array finishes first 
         temp_arr[start_index] = arr[ptr_1];
         start_index++;
         ptr_1++;
     } 
 
     
-    while(ptr_2 <= high){
-        temp_arr[start_index] = arr[ptr_2];
-        start_index++;
+    while(ptr_2 <= high){  //! if the right array finishes first 
+        temp_arr[start_index] = arr[ptr_2]; //! then take all ements of left array(already sorted ) and put to full array
+        start_index++; 
         ptr_2++;
     }
-    start_index = 0;
 
-    for(int idx = low;idx<=high;idx++){
+    start_index = 0;//! just repositioned the indicies of temp arr to 0
+    for(int idx = low;idx<=high;idx++){   //! to add Sorted array(hupothetical) into Original Array 
         arr[idx] = temp_arr[start_index];
         start_index++;
     }
@@ -60,13 +61,14 @@ int main()
     // }
 
     for(int x : arr){
-        cout<<x<<endl;
+        cout<<x<<" ";
     }
+    cout<<endl;
 
     merge(arr, 0, n - 1);
 
     for(int x : arr){
-        cout<<x<<endl;
+        cout<<x<<" ";
     }
 
 }
