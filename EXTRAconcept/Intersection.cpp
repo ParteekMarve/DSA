b13f2ccffa61113e1a1ae4d7fb75243c7bf608eb
marve01-hub <parteek.marve09@gmail.com>
2025-10-25 02:55:06 +0530

    Intersection by hashmap ***

--- EXTRAconcept/Intersection.cpp
+++ EXTRAconcept/Intersection.cpp
@@ -1,28 +1,21 @@
-
-//2, 2, 1, 4, 5, 6, 2
-
-//2, 1, 2, 3, 4, 6
-//result => 2, 2, 1 , 4, 6
 #include<iostream>
-#include<set>
+#include<unordered_map>
 using namespace std;
 int main(){
 
-    set<int> s;
+    unordered_map<int,int> freq;
 
-    int a1[6] = {2, 2, 1, 4, 5, 6};
-    int a2[6] = {2, 2, 2, 3, 4, 6};
-    for(int i = 0;i<6;i++){
-        if(a1[i]!=a1[i+1])
-        s.insert(a1[i]);
-    }
-    for(int i = 0;i<6;i++){
-        s.insert(a2[i]);
-    }
-    for(auto &x: s){
-        cout<<x<<" ";
-        
-    }
+    int a1[6] = {2, 5, 6, 2, 8, 10};
+    int a2[6] = {1, 2, 8, 2, 9, 10};
     
+    for(int  i=0;i<6;i++){
+        freq[a1[i]]++;
+    }
 
+    for(int i= 0;i<6;i++){
+        if(freq[a2[i]]>0){  //! checks the presence of current elmt in map through its freq => if freq >0 means elnt exists and need to print
+            cout<<a2[i]<<" ";
+            freq[a2[i]]--;   //! If current elmt exists in the map, print it and decrease its count
+        }
+    }
 }
