544bc2ded0e3a430cb9653dba4380f52fa023c84
marve01-hub <parteek.marve09@gmail.com>
2025-10-29 18:53:11 +0530

    Swaping each elememnt by 1 plsce

--- ARRAYS/EASY/SwapbyLeft.cpp
+++ ARRAYS/EASY/SwapbyLeft.cpp
@@ -2,14 +2,18 @@
 using namespace std;
 int main(){
     int n =5;
-    int k;
-    cout<<"enter k: ";
-    cin>>k;
     int arr[5] = {1,2,3,4,5};
        //! T.C. = O(n) 
-        for(int i = 0;i<=n-2;i++){
-        swap(arr[i],arr[i+1]);
+    //     for(int i = 0;i<=n-2;i++){
+    //     swap(arr[i],arr[i+1]);
+    // }
+    //?     OR
+
+    int temp = arr[0];
+    for(int i =1;i<n;i++){
+        arr[i-1] = arr[i];
     }
+    arr[n-1] = temp;
     
     
     for(int i=0;i<n;i++){
