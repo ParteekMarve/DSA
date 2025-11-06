cb54a0b0f23ce7c11e710daa00cf09b0c3c190e2
marve01-hub <parteek.marve09@gmail.com>
2025-11-07 01:32:05 +0530

    easy one **

---
+++ ARRAYS/MEDIUM/TwoSum.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<unordered_map>
+#include<algorithm>
+using namespace std;
+// bool found(int arr[],int n,int tarnum){
+//     bool pairfound = false;
+//     for (int i =0;i<n-1;i++){
+//         for(int j = i+1;j<n;j++){
+//             if(arr[i] + arr[j] == tarnum){
+//                 cout << "Pair found: " << arr[i] << " + " << arr[j] << " = " << tarnum << endl;
+//                 pairfound  = true;
+//                 break;
+//             }
+//         }
+//         if(!pairfound){
+//             cout<< "no pair found";
+//         }
+//     }
+//     return pairfound;
+// }
+
+//bool found_2nd(int arr[], int n, int tarnum) {
+//     unordered_map<int, int> mpp;
+//     for (int i = 0; i < n; i++) {
+//         int temp = tarnum - arr[i];
+//         if (mpp.count(temp)) {
+//             cout << temp << " " << arr[i] << endl;
+//             return true;
+//         }
+//         mpp[arr[i]] = i;  // insert after checking
+//     }
+//     return false;
+// }
+    
+
+int main(){
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    int tarnum;
+    cout<<"Enter num: ";
+    cin>>tarnum;
+    int arr[n];
+    for(int i= 0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr, arr+n);
+    // found(arr,n,tarnum);
+    // found_2nd(arr,n,tarnum);
+    // found_3rd(arr,n,tarnum);
+    int left  = 0;
+    int right = n-1;
+    while(left< right){
+        int sum = arr[left] + arr[right];
+        if(sum == tarnum){
+            cout<< arr[left] << arr[right];
+            return 0;  //! if we use [break] here then we have to add another flag like bool found = true || by using return 0 it directly exits and goes to the main function
+        }
+        else if(sum < tarnum) left++;
+        else right--;
+    }
+    cout<< "Not found!";   
+}
