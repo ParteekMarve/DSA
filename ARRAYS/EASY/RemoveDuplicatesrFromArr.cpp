ebc8499e681895ca5cb0b3bfbb147ef4f0e59247
Parteek Marve <parteek.marve09@gmail.com>
2025-11-10 17:48:00 +0530

    Refactor removeDuplicates to use two-pointer technique
    Refactored the removeDuplicates function to eliminate the use of a set and instead use a two-pointer technique for in-place modification of the input array.

--- ARRAYS/EASY/RemoveDuplicatesrFromArr.cpp
+++ ARRAYS/EASY/RemoveDuplicatesrFromArr.cpp
@@ -5,22 +5,34 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        set<int> s;    // as set contains only unique and sorted elements
-        for(int i = 0;i<nums.size();i++){  // due to which no duplicates will be present here
-            s.insert(nums[i]);
+    if (nums.empty()) return 0;
+    int i = 0;
+    for (int j = 1; j < nums.size(); j++) {
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
         }
-        int idx = 0;
-        for(auto it: s){
-            nums[idx] = it;   // putting unique element vali array into nums(original array)
-            idx++;
-        }
-
-        // let nums = [1, 1, 2, 2, 3]
-
-        //After inserting into set: s = {1, 2, 3}
-        return idx; //! returns no of unique elmts    //Then nums becomes: [1, 2, 3, 2, 3]  1st 3(no of unique elmts) elemnts ki jagah only set vale aage without changing order of further elemts 
-        
     }
+    return i + 1;
+}
+
+    // int removeDuplicates(vector<int>& nums) {
+    //     set<int> s;    // as set contains only unique and sorted elements
+    //     for(int i = 0;i<nums.size();i++){  // due to which no duplicates will be present here
+    //         s.insert(nums[i]);
+    //     }
+    //     int idx = 0;
+    //     for(auto it: s){
+    //         nums[idx] = it;   // putting unique element vali array into nums(original array)
+    //         idx++;
+    //     }
+
+    //     // let nums = [1, 1, 2, 2, 3]
+
+    //     //After inserting into set: s = {1, 2, 3}
+    //     return idx; //! returns no of unique elmts    //Then nums becomes: [1, 2, 3, 2, 3]  1st 3(no of unique elmts) elemnts ki jagah only set vale aage without changing order of further elemts 
+        
+    // }
 };
 
 int main() {
