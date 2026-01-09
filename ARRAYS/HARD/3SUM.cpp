50601374c639ec36aa4982cdbdf71336f86dd5c5
Parteek Marve <parteek.marve09@gmail.com>
2026-01-09 17:46:28 +0530

    **TOP NOTCH CONCEPT**

--- ARRAYS/HARD/3SUM.cpp
+++ ARRAYS/HARD/3SUM.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <unordered_set>
 #include <algorithm>
 using namespace std;
 
@@ -9,23 +10,52 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
         set<vector<int>> unq;
+        //! OPTIMAL {OPTIMAL}
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
         
-        //! BETTER APPROACH {LOOP 2 TIMES AND TRY TO FIND 3RD ELEMENT use hashset}
+        // *OPTIMAL* {2 POINTER}
         for(int i = 0; i < n; i++){
-            set<int> hashset; // stores the seen values in current iteration
-            for(int j = i + 1; j < n; j++){
-                int thirdele = -(nums[i] + nums[j]);
-                // if 3rd element already in set then add all 3 elements to vector
-                if(hashset.find(thirdele) != hashset.end()){
-                    vector<int> temp = {nums[i], nums[j], thirdele};
-                    sort(temp.begin(), temp.end());
-                    unq.insert(temp); 
+            // checking for duplicate and this only happens when the fixed element is at any index except the 0th bcz for 0th idx we have no need to check
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            
+            // 2 pointers for the other two elements
+            int left = i + 1;
+            int right = n - 1;
+            
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0) {
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    left++; // continue search for the next triplets
+                    right--;
+                    
+                    // Skipping the duplicate for both the 2 elements while the condition is met
+                    while(left < right && nums[left] == nums[left-1]) left++;
+                    while(left < right && nums[right] == nums[right+1]) right--;
                 }
-                // else add current element (nums[j]) to hashset for future lookups
-                hashset.insert(nums[j]);
+                else if(sum > 0) right--;
+                else left++;
             }
         }
-       //! BRUTE {LOOP 3 TIMES TO ADD EACH NUMBER AND CHECK}
+        return ans;
+        
+        // //! BETTER APPROACH {LOOP 2 TIMES AND TRY TO FIND 3RD ELEMENT use hashset}
+        // for(int i = 0; i < n; i++){
+        //     unordered_set<int> hashset; // stores the seen values in current iteration [more efficient than using set here]
+        //     for(int j = i + 1; j < n; j++){
+        //         int thirdele = -(nums[i] + nums[j]);
+                 // if 3rd element already in set then add all 3 elements to vector
+        //         if(hashset.find(thirdele) != hashset.end()){
+        //             vector<int> temp = {nums[i], nums[j], thirdele};
+        //             sort(temp.begin(), temp.end());
+        //             unq.insert(temp); 
+        //         }
+                 // else add current element (nums[j]) to hashset for future lookups
+        //         hashset.insert(nums[j]);
+        //     }
+        // }
+        //! BRUTE {LOOP 3 TIMES TO ADD EACH NUMBER AND CHECK THE CONDITION}
         // for (int i = 0; i < n; i++) {
         //     for (int j = i + 1; j < n; j++) {
         //         int sum = 0;
@@ -39,8 +69,8 @@ public:
         //     }
         // }
         // Converting Set into vector as we need to return vector of vectors
-        vector<vector<int>> ans(unq.begin(), unq.end());
-        return ans;
+        // vector<vector<int>> ans(unq.begin(), unq.end());
+        // return ans;
     }
 };
 
