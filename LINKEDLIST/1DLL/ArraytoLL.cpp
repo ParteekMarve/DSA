6c43bb238427f1bca28b6ade172b46cc65d65766
Parteek Marve <parteek.marve09@gmail.com>
2026-03-11 22:29:07 +0530

    Arr2LL

--- LINKEDLIST/1DLL/ArraytoLL.cpp
+++ LINKEDLIST/1DLL/ArraytoLL.cpp
@@ -23,9 +23,9 @@ Node* convertArr2LL(vector<int> &arr){
     Node* mover = head;
     // traverse across rest of the array
     for (int i = 1;i<arr.size();i++){
-        Node* temp = new Node(arr[i]);
-        mover->next = temp;
-        //or
+        Node* temp = new Node(arr[i]); // !Creates new node for each array element
+        mover->next = temp; //? Links current last node to new node
+                            //? mover is just a COPY of head's address      
         mover = temp;
     }
     return head; // bcz in linked list we are asked to return head of the LL not the whole LL
