206d08019f69e659b0f085595ca908d5b9a6829c
Parteek Marve <parteek.marve09@gmail.com>
2026-03-12 18:07:27 +0530

    Traverse|Compare

---
+++ LINKEDLIST/1DLL/SearchforVal.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+//? Node class representing a Node of a Linked List
+class Node {
+    public:
+        int data; // data value
+        Node* next; // pointer to next node
+
+        //! Constructor for data and next(pointer){Initialization}
+        Node(int data1,Node* next1){
+            data = data1;
+            next = next1;
+        }
+        //! Constructor for data only(representing Null(end)){Initialization}
+        Node(int data1){
+            data = data1;
+            next = nullptr;
+        }
+};
+Node* convertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    // traverse across rest of the array
+    for (int i = 1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]); // !Creates new node for each array element
+        mover->next = temp; //? Links current last node to new node
+                            //? mover is just a COPY of head's address      
+        mover = temp;
+    }
+    return head; // bcz in linked list we are asked to return head of the LL not the whole LL
+}
+
+bool valPresentorNot(Node* head,int val){
+    //? means while traversing the list we have to check that at any node has data == val or not  
+    Node* temp = head;
+    while(temp!= nullptr){
+        if(temp->data == val){
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
+int main(){
+    vector<int> arr = {1,2,3,4};
+    Node* head = convertArr2LL(arr);
+    cout<< valPresentorNot(head,12);
+        
+}
---
+++ LINKEDLIST/1DLL/TraversingLL.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+//? Node class representing a Node of a Linked List
+class Node {
+    public:
+        int data; // data value
+        Node* next; // pointer to next node
+
+        //! Constructor for data and next(pointer){Initialization}
+        Node(int data1,Node* next1){
+            data = data1;
+            next = next1;
+        }
+        //! Constructor for data only(representing Null(end)){Initialization}
+        Node(int data1){
+            data = data1;
+            next = nullptr;
+        }
+};
+Node* convertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    // traverse across rest of the array
+    for (int i = 1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]); // !Creates new node for each array element
+        mover->next = temp; //? Links current last node to new node
+                            //? mover is just a COPY of head's address      
+        mover = temp;
+    }
+    return head; // bcz in linked list we are asked to return head of the LL not the whole LL
+}
+int lengthofLL(Node* head){
+    Node* temp = head;
+    int count = 0;
+    while(temp!= nullptr){
+        temp = temp->next;
+        count++;
+    }
+    return count;
+}
+
+int main(){
+    vector<int> arr = {1,2,3,4};
+    Node* head = convertArr2LL(arr);
+    // TRAVERSING ARRAY
+    // Node* temp = head; // start from head
+    // while(temp!= nullptr){
+    //     cout<< temp->data;
+    //     temp = temp->next;
+    // }
+    //Finding length of array
+    cout<<lengthofLL(head);
+        
+}
