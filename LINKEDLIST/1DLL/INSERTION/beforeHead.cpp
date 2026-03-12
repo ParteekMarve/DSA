27d76aa3c15c096062361ee95d65bdfd94d9a9f2
Parteek Marve <parteek.marve09@gmail.com>
2026-03-12 23:12:33 +0530

    return temp =New node

---
+++ LINKEDLIST/1DLL/INSERTION/beforeHead.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* next;
+
+        Node(int data1, Node* next1){
+            data = data1;
+            next = next1;
+        }
+        Node(int data1){
+            data = data1;
+            next = nullptr;
+        }
+};
+
+Node* convertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for (int i = 1; i < arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+//! New Node In Front of Head making it as new head
+Node* placeNewHead(Node* head,int val){
+    Node* temp = new Node(val,head);
+    return temp; // as a new head
+}
+
+//  Helper to print entire linked list
+void printLL(Node* head){
+    Node* temp = head;
+    while(temp != nullptr){
+        cout << temp->data;
+        if(temp->next != nullptr) cout << " -> ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main(){
+    vector<int> arr = {1,2,3,4};
+    Node* head = convertArr2LL(arr);
+    head = placeNewHead(head,0);
+    printLL(head);
+    
+    
+}
