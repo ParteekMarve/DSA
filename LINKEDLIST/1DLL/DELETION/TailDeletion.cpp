a9b6c0c6e3b4ed7ac63fd7295e8e9a2be2704067
Parteek Marve <parteek.marve09@gmail.com>
2026-03-12 20:11:10 +0530

    check temp.next.next

---
+++ LINKEDLIST/1DLL/DELETION/TailDeletion.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* next;
+
+        Node(int data1, Node* next1){
+            data = data1;
+            next = next1;
+        }
+        Node(int data1){
+            data = data1;
+            next = nullptr;
+        }
+};
+
+Node* convertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for (int i = 1; i < arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+Node* removeTail(Node* head){
+    if(head == NULL || head->next == NULL) return NULL;
+    Node* temp = head;
+    while(temp->next->next != NULL){
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = nullptr;
+    return head;
+}
+
+// ✅ Helper to print entire linked list
+void printLL(Node* head){
+    Node* temp = head;
+    while(temp != nullptr){
+        cout << temp->data;
+        if(temp->next != nullptr) cout << " -> ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main(){
+    vector<int> arr = {1,2,3,4};
+    Node* head = convertArr2LL(arr);
+    
+    cout << "Before: ";
+    printLL(head);          // 1 -> 2 -> 3 -> 4
+    
+    head = removeTail(head);
+    
+    cout << "After:  ";
+    printLL(head);          // 1 -> 2 -> 3
+    return 0;
+}
