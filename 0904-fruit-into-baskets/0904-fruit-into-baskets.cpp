91de4a3d5d27506074ee59f834e2834e0d17f023
Parteek Marve <parteek.marve09@gmail.com>
2026-09-26 00:14:19 +0530

    Time: 46 ms (46.18%), Space: 84.7 MB (83.08%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -2,48 +2,21 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         int n = fruits.size();
-
-        //  ultimaltely what matters is from where u start
-        // keep an unorederd set that will contain at max 2 distinct elements at any time
-        // whenever it will have more than 2 elements we will delete the leftmost 
-        int maxlen = 0;
+        int max_fruits = 0;
+        unordered_map<int,int> basket;
         int l = 0;
-        unordered_map<int,int> mpp;
-        for(int r = 0;r<n;r++){ 
-            mpp[fruits[r]]++;
-            while(mpp.size()>2){
-                // keep removing from last
-                mpp[fruits[l]]--;
-                if(mpp[fruits[l]] == 0){
-                    mpp.erase(fruits[l]);
+        for(int r = 0;r<n;r++){
+            basket[fruits[r]]++;
+            while(basket.size() > 2){
+                basket[fruits[l]]--;
+                if(basket[fruits[l]] == 0){
+                    basket.erase(fruits[l]);
                 }
                 l++;
             }
-            
-            maxlen = max(maxlen,r-l+1);
+            max_fruits = max(max_fruits,r-l+1);
         }
 
-        return maxlen;
-
+        return max_fruits;
     }
-    // int totalFruit(vector<int>& fruits) {
-    //     int n = fruits.size();
-
-    //     //  ultimaltely what matters is from where u start
-    //     // keep an unorederd set that will contain at max 2 distinct elements at any time
-    //     // whenever it will have more than 2 elements we will delete the leftmost 
-    //     int maxlen = 0;
-    //     for(int l = 0;l<n;l++){
-    //         unordered_map<int,int> mpp;
-    //         for(int r = l;r<n;r++){
-    //             mpp[fruits[r]]++;
-    //             if(mpp.size()>2){
-    //                 break;
-    //             }
-    //             maxlen = max(maxlen,r-l+1);
-    //         }
-    //     }
-    //     return maxlen;
-
-    // }
 };
